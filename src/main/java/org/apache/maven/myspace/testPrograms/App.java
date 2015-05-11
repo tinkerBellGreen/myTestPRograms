a49e1fc42ff472c6a3b1a253e74f55b70439ca0d
namita <namita_mahtta11@yahoo.co.in>
2015-05-10 23:52:40 -0600

    Just testing git project

---
+++ src/main/java/org/apache/maven/myspace/testPrograms/App.java
@@ -0,0 +1,18 @@
+package org.apache.maven.myspace.testPrograms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private void findingAcuteAngle(int hourHand, int minuteHand){
+    	int angleInOneSec = 6;
+    	int angleInOneMin = 6;
+    	int angleHourHand = 30;
+    }
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
